7da65e5a2890c55e15c2da0d64ead165af1a3d47
Nestor Bonilla <nmora@delbridge.solutions>
2023-08-17 21:50:52 -0600

    refactor: :tada: WIP

---
+++ deploy/development/src/Development/DevelopmentStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Development
+{
+    public class DevelopmentStack : Stack
+    {
+        internal DevelopmentStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
